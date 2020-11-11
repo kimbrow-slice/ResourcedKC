b8c7df791107ed9e6a5d8d5a6bdeeedac9a54f3d
Jeffery Kimbrow <71730781+KimbrowSkilledKC@users.noreply.github.com>
2020-11-11 15:07:30 -0600

    skeleton

---
+++ index.js
@@ -0,0 +1,62 @@
+class Location {
+    constructor(city, zip, state) {
+        this.zip = zip;
+        this.city = city;
+        this.state = null;
+    }
+}
+
+class Resource {
+    constructor() {
+        this.name = null;
+        //a name
+        //phone 
+        //government vs nonprofit
+        //demographic info supports
+        //this.location new Location()
+    }
+}
+
+class Topic {
+    constructor() {
+        //topic name
+        //resources [Resource]
+        //demographic 
+    }
+}
+
+class DemographicInfo {
+
+}
+
+class Client {
+    constructor(topics, demoinfo, location) {
+        this.demoinfo = new DemographicInfo();
+        this.topics = new Topic();
+        this.location = new Location();
+    }
+
+    //     getresources(){
+    //         /* make a function to be able to retrieve resources
+    //         based on client interests and location */ 
+    //      for( every topic we're interested in )
+    //       if(this.location === topic[i].resource.location ) {
+    //         listOfResources.push(topic[i].resource); 
+    //       }
+
+    //       return listofresource 
+    //     }
+    //   }
+
+    //   topic(homelessness)
+    //     The Awesome Shelter = 66101
+    //     The lgbt support Center = 66101 
+
+    //   I am here = 66101 
+
+    //   //UI function 
+    //   function displayResrouces(){ 
+    //     innerhtml = resourcesForLocationAndTopic() 
+    //   }
+
+    //   let activeClient = new Client();
