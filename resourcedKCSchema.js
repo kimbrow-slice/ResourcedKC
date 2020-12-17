2ae5668284b07c587bfc013d1c0365da0619a8d2
Jeffery Kimbrow <71730781+kimbrow-slice@users.noreply.github.com>
2020-12-17 16:47:27 -0600

    edit

--- resourcedKCSchema.js
+++ resourcedKCSchema.js
@@ -65,16 +65,8 @@ db.once('open', function(){
     });
 
 
-       /* adminLogin schema for the administration to be able to update their organizations business information if need be
-         When developing the documents we plan to use JSON because of two key factors. The first being that JSON has faster Parsing than XML, with the large amounts of memory used due to the verbosity and cost of parsing large XML files. JSON is better for preformance because it consumes very little memory space. The second being with the fact our data types are only going to be text and numbers and not using images, charts, or graphs held within our database specifcally. */
-
-        const userSchema = new mongoose.Schema({
-            username : {type: String, required: true, maxlength: 25},
-            password : {type: String, required: true, minlength: 8},
-            emailAddress : {type: String, required: true, maxlength: 150},
-            admin : {type: Boolean, required: true}
-        });
-    
+        
+        
                     // ( 4.5 Weeks is === 252 Hours of working time )
 });
 
